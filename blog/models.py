000bf8c1270d033555182ff474c17cf8aef7cb9e
bingbuyu <1185932667@qq.com>
2017-03-24 19:07:55 +0800

    1

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.utils import timezone
-from ckeditor.fields import RichTextField
-from ckeditor_uploader.fields import RichTextUploadingField
 
 class Category(models.Model):
     name = models.CharField(max_length=16)
