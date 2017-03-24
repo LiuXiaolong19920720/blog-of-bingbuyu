5d6a467b2e7edc71574c718ec87300df7240067c
bingbuyu <1185932667@qq.com>
2017-03-24 19:10:05 +0800

    2

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url,include
 from . import views
-import ckeditor
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
@@ -9,5 +8,4 @@ urlpatterns = [
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
     
     url(r'^like-blog/$', views.like_count_blog, name='like_count_blog'),
-    #url(r'ckeditor/',include(ckeditor.urls)),
 ]
